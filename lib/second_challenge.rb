631442782bc382d6cd06ef8785c654ed62541e15
Evan  Markert <evanmarkert@gmail.com>
2017-12-05 16:40:46 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,18 +1,18 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriach => {}, 
-      :matriarch => {}, 
-      :hero => {}, 
+      :patriach => {},
+      :matriarch => {},
+      :hero => {},
       :hero_friends => []
-      }, 
+      },
     :capulet => {
-      :patriach => {}, 
-      :matriarch => {}, 
-      :hero => {}, 
+      :patriach => {},
+      :matriarch => {},
+      :hero => {},
       :heroine_friends => []
       }
     }
 
-  
+
 end
